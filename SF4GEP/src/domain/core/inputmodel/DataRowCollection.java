55d9009b7ad17fa3f42500393a387d28925f29fa
zwein27@gmail.com <zwein27@gmail.com@0f924ae1-d416-614b-8bf3-34ffd1195aa7>
2012-07-06 15:01:00 +0000

    DataTable加到InputModel，添加了一点注释，完善中

---
+++ SF4GEP/src/domain/core/inputmodel/DataRowCollection.java
@@ -0,0 +1,46 @@
+package domain.core.inputmodel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DataRowCollection {
+     DataColumnCollection columns;
+     List<DataRow> dataRows;
+     public DataRowCollection(){
+   	  columns=new DataColumnCollection();
+   	  dataRows=new ArrayList<DataRow>();
+     }
+     public DataRowCollection(List<DataRow> rows,DataColumnCollection columns){
+    	 this.columns=columns;
+      	  this.dataRows=rows;
+     }
+     public void add(int index,DataRow row){
+    	 if(index<dataRows.size())
+    	  dataRows.add(index,row);
+    	 else
+    	  dataRows.add(dataRows.size(),row);
+      }
+      public void add(DataRow row){
+    	  dataRows.add(row);
+      }
+      public void add(Object obj){
+    	  dataRows.add((DataRow) obj);
+      }
+      public void add(int index,Object obj){
+    	  dataRows.add(index,(DataRow) obj);
+      }
+	public int size() {
+		
+		return dataRows.size();
+	}
+	public DataRow get(int index) {
+		
+		return dataRows.get(index);
+	}
+	public void setColumns(DataColumnCollection columns2) {
+		this.columns=columns2;
+		
+	}
+     
+}
---
+++ SF4GEP/src/domain/core/inputmodel/DataTable.java
@@ -0,0 +1,209 @@
+package domain.core.inputmodel;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+public final class DataTable {
+	
+	/**
+	 * 保存行的集合
+	 * 用来保存DataRowCollection的集合
+	 */
+	private DataRowCollection rows; 
+	
+	/**
+	 * 保存列的集合
+	 * 用于保存DataColumn的对象
+	 */
+	private DataColumnCollection columns; 
+	
+	/**
+	 * 保存Table的名字
+	 */
+	private String tableName; //表名
+	
+	/**
+	 * 设置是否只读
+	 * 默认false
+	 */
+    private boolean readOnly = false;
+    
+    /**
+     * 下一行的引用
+     * 初始值为0
+     */
+	private int nextRowIndex = 0;
+	
+	/**
+	 * 标记对象
+	 */
+	private Object tag;
+	
+	/**
+	 * 默认的构造函数
+	 * 初始化 行的集合、列的集合
+	 */
+	public DataTable() {
+		  this.columns = new DataColumnCollection();
+		  this.rows = new DataRowCollection();
+		  this.rows.setColumns(columns);
+    }
+	
+	/**
+	 * 设置表的名称
+	 * @param dataTableName
+	 */
+	public DataTable(String dataTableName) {
+          this();
+		  this.tableName = dataTableName;
+    }
+	
+	
+	/**
+	 * 获取中的行数
+	 * @return
+	 */
+	public int getTotalCount() {
+          return rows.size();
+	} 
+	
+	
+	/**
+	 * 判断是否只读
+	 * @return
+	 */
+    public boolean isReadOnly() {
+		  return this.readOnly;
+    }   
+    
+    /**
+     * 设置是否只读
+     * @param readOnly
+     */
+	public void setReadOnly(boolean readOnly) {
+		  this.readOnly = readOnly;
+	}
+	
+	/**
+	 * 获取表名
+	 * @return
+	 */
+	public String getTableName() {
+         return this.tableName;
+	}
+	
+	/**
+	 * 设置表名
+	 * @param tableName
+	 */
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+	}
+    
+    
+    /**
+     * 获得行的集合
+     * @return
+     */
+    public DataRowCollection getRows() {
+            return this.rows;
+     }
+    
+    /**
+     * 获得列的集合
+     * @return
+     */
+    public DataColumnCollection getColumns() {
+    	    return this.columns;
+    }
+    
+    
+    
+    /**
+     * 获取给定行号的给定列名的值
+     * @param row	行号
+     * @param colName	列名
+     * @return
+     */
+    public Object getValue(int row,String colName) {
+    		return this.rows.get(row).getValue(colName);
+    }
+    
+    
+    
+    /**
+     * 获取给定行号和列号的值
+     * @param row
+     * @param col
+     * @return
+     */
+    public Object getValue(int row,int col) {
+    	     return this.rows.get(row).getValue(col);
+    }
+    public DataRow newRow() throws Exception {
+             DataRow tempRow = new DataRow(this);
+             nextRowIndex = nextRowIndex < this.rows.size() ? this.rows.size(): nextRowIndex;
+    	     tempRow.setColumns(this.columns);
+    	     tempRow.setRowIndex(nextRowIndex++);
+    	     return tempRow;      
+    }
+    public void setValue(int row,int col,Object value) {
+             this.rows.get(row).setValue(col, value);
+    }
+       
+    public void setValue(int row,String colName,Object value) {
+    		 this.rows.get(row).setValue(colName, value);
+    }
+    public Object getTag() {
+		return tag;
+	}
+	public void setTag(Object tag) {
+		this.tag = tag;
+	}
+	public DataColumn addColumn(String columnName,
+        int dataType) throws Exception {
+		return this.columns.addColumn(columnName, dataType);
+	}
+	public void addRow(DataRow row) throws Exception {
+		if (row.getRowIndex() > this.rows.size())
+		    row.setRowIndex(this.rows.size());
+			 this.rows.add(row);
+	}
+    public String getColumnName(int index){
+    	
+		return columns.get(index).getColumnName();
+    	
+    }
+   public String getColumnName(Object obj){
+	 for(int i=0;i<this.rows.size();i++){
+	     DataRow row=this.rows.get(i);
+	     //System.out.println("ww"+row.getValue(i));
+	     for(int j=0;j<this.columns.size();j++){
+	    	 Object obj2=this.getValue(i, j);
+	    	 //System.out.println("obj2"+obj2.toString()+"  "+obj.toString());
+	    	 if(obj2.toString().equals(obj.toString())){
+		    	  return row.getColumns().get(j).getColumnName().toLowerCase();
+		     }
+	     }
+	    
+    	 
+    	
+	   }	
+	 return "没有找到";		
+   }
+   /* Iterator it=columns.nameMap.entrySet().iterator();
+    * while(it.hasNext()){
+		Entry entry=(Entry) it.next();
+		System.out.println("ww"+entry.getKey());
+		if(entry.getValue().equals(obj)){
+			return (String) entry.getKey();
+		}
+		
+	}*/
+public void addColumn(String  columnName) {
+	this.columns.addColumn(columnName);
+	
+}
+
+
+
+}
