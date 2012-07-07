357593e59741f753bd0d5e15ff2f02fcc9a9e55b
shenyuancs@gmail.com <shenyuancs@gmail.com@0f924ae1-d416-614b-8bf3-34ffd1195aa7>
2012-07-07 02:52:34 +0000

    添加了注释

--- SF4GEP/src/domain/core/algmodel/configuration/Population.java
+++ SF4GEP/src/domain/core/algmodel/configuration/Population.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 import common.ICopy;
 
-
+/**
+ * 种群实体类
+ * @author 申远
+ *
+ */
 public class Population implements Serializable,ICopy<Population>{
 	/**
 	 * 
@@ -15,19 +19,39 @@ public class Population implements Serializable,ICopy<Population>{
 	private int size;
 	private List<Individual> individuals;
 	private long generation;
+	/**
+	 * 设置种群所包含的个体
+	 * @param individuals 种群中个体的List
+	 */
 	public void setIndividuals(List<Individual> individuals) {
 		this.individuals = individuals;
 	}
+	/**
+	 * 创建一个种群，用户需要提供种群大小
+	 * @param size 种群大小
+	 */
 	public Population(int size){
 		this.size=size;
 		individuals=new ArrayList<Individual>(size);
 	}
+	/**
+	 * 创建种群，用户提供种群中所有个体组成的List
+	 * @param popuList 包含有种群中所有个体的List
+	 */
 	public Population(List<Individual> popuList){
 		this.individuals=popuList;
 	}
+	/**
+	 * 返回种群中所有的个体
+	 * @return 种群中所有个体组成的List
+	 */
 	public List<Individual> getIndividuals(){
 		return individuals;
 	}
+	/**
+	 * 返回种群中最优个体
+	 * @return 种群中最优个体
+	 */
 	public Individual getBestIndividual(){
 		Individual bestIndividual=individuals.get(0);
 		for(Individual element:individuals)
@@ -35,6 +59,10 @@ public class Population implements Serializable,ICopy<Population>{
 				bestIndividual=element;
 		return bestIndividual;
 	}
+	/**
+	 * 返回种群中最差个体
+	 * @return 种群中最差个体
+	 */
 	public Individual getWorstIndividual(){
 		Individual bestIndividual=individuals.get(0);
 		for(Individual element:individuals)
@@ -42,15 +70,31 @@ public class Population implements Serializable,ICopy<Population>{
 				bestIndividual=element;
 		return bestIndividual;
 	}
+	/**
+	 * 返回当前种群的代数，即当前种群是第几代种群
+	 * @return 当前种群的代数
+	 */
 	public long getGeneration(){
 		return generation;
 	}
+	/**
+	 * 设置当前种群的代数，即设置当前种群是第几代种群
+	 * @param generation 当前种群的代数
+	 */
 	public void setGeneration(long generation) {
 		this.generation = generation;
 	}
+	/**
+	 * 向种群中添加个体
+	 * @param individual 待添加的个体
+	 */
 	public void addIndividual(Individual individual){
 		individuals.add(individual);
 	}
+	/**
+	 * 对当前种群进行复制，产生一个新的种群，对于string 这样的不可改变对象和int这样的基本数据类型，这是一个浅复制，对于其他的对象而言，这是一个深度复制
+	 * @return 一个新的种群，二者不共享内存空间，只是具有相同的初始化的值
+	 */
 	@Override
 	public Population copy() {
 		// TODO Auto-generated method stub
