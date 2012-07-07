3105cb45879432e0fe8b57a88e897c50f811bef2
shenyuancs@gmail.com <shenyuancs@gmail.com@0f924ae1-d416-614b-8bf3-34ffd1195aa7>
2012-07-07 04:19:54 +0000

    添加了注释

--- SF4GEP/src/domain/core/algmodel/individualcomponent/HomeoticGene.java
+++ SF4GEP/src/domain/core/algmodel/individualcomponent/HomeoticGene.java
@@ -4,13 +4,20 @@ import java.util.List;
 
 import domain.core.algmodel.genecomponent.GenePiece;
 
+/**
+ * 同源基因实体类
+ * @author 申远
+ * 
+ */
 public class HomeoticGene extends Gene{
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 4297205056477494966L;
 	/**
-	 * 
+	 * 创建同源基因的构造函数，用户需要在当前基因对应的个体的全部基因片段中指定当前基因从基因片段的哪里开始，哪里结束。
+	 * @param headerLength 同源基因的头部长度
+	 * @param tailLength 同源基因的尾部长度
+	 * @param genePieces 个体中所包含的全部基因片段
+	 * @param start 当前基因在个体基因片段中开始的地方，包括这个地址
+	 * @param end 当前基因在个体基因片段中结束的地方，不包括这个地址
 	 */
 	public HomeoticGene(int headerLength,int tailLength,List<GenePiece> genePieces,int start,int end){
 		super(genePieces,start,end);
