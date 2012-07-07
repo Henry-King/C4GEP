9cb6f86706283397ab35cc013de96629b47f24fa
shenyuancs@gmail.com <shenyuancs@gmail.com@0f924ae1-d416-614b-8bf3-34ffd1195aa7>
2012-07-07 05:08:01 +0000

    添加了注释

--- SF4GEP/src/domain/core/algmodel/genecomponent/GenePiece.java
+++ SF4GEP/src/domain/core/algmodel/genecomponent/GenePiece.java
@@ -4,27 +4,45 @@ import java.io.Serializable;
 
 import common.ICopy;
 
-
+/**
+ * 基因片段类，或称基因位类，在每一个头部或者尾部中，最终包含的都是此类GenePiece
+ * @author 申远
+ *
+ */
 public abstract class GenePiece implements Serializable,Cloneable,ICopy<GenePiece>{
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 7648117930489664221L;
 	protected String symbol;
-	
+	/**
+	 * 返回当前基因片段的名称，用String表示
+	 * @return 基因片段名称
+	 */
 	public String getSymbol() {
 		return symbol;
 	}
-
+	/**
+	 * 设置当前基因片段的名称，用String表示
+	 * @param symbol 基因片段名称
+	 */
 	public void setSymbol(String symbol) {
 		this.symbol = symbol;
 	}
+	/**
+	 * 返回当前基因片段的字符串表示，本方法将直接调用getSymbol
+	 */
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
 		return symbol;
 	}
+	/**
+	 * 获得当前基因片段的值
+	 * @return 当前基因片段的值
+	 */
 	public abstract float getValue();
+	/**
+	 * 克隆一份当前对象，这个方法调用了Object中的clone方法。
+	 * 这是一个深度克隆，即源对象和新对象具有相同的初值，但具有不同的内存地址。
+	 */
 	@Override
 	public GenePiece clone(){
 		// TODO Auto-generated method stub
@@ -38,6 +56,9 @@ public abstract class GenePiece implements Serializable,Cloneable,ICopy<GenePiec
 		return o;
 	}
 	@Override
+	/**
+	 * 这个copy方法将直接调用clone方法实现深度复制
+	 */
 	public GenePiece copy() {
 		// TODO Auto-generated method stub
 		return clone();
