d8be47e55ce713bfc89d6db37fae891ea435219c
shenyuancs@gmail.com <shenyuancs@gmail.com@0f924ae1-d416-614b-8bf3-34ffd1195aa7>
2012-07-13 01:47:29 +0000

    IService,与原来的集成依然会有编译错误

--- SF4GEP/src/domain/iservice/algConfiguration/IgepConfigurationService.java
+++ SF4GEP/src/domain/iservice/algConfiguration/IgepConfigurationService.java
@@ -1,8 +1,20 @@
 package domain.iservice.algConfiguration;
 
+import domain.core.algInputDataProcess.DataSet;
 import domain.core.algconfiguration.GepAlgConfiguration;
 
 public interface IgepConfigurationService {
-	public GepAlgConfiguration getGepAlgConfigurationByName();
+	public GepAlgConfiguration getGepAlgConfigurationByName(String name);
 	public boolean saveGepAlgConfiguration(GepAlgConfiguration gepAlgConfiguration);
+	/**
+	 * 配置信息中有些属性有简单的函数决定关系，例如b＝a+1，调用前只需要填充b，此方法会自动计算a的值
+	 * 前置条件：调用此方法前，请确定GepAlgConfiguration的所有子属性均填充完整。因此在正式使用GepAlgConfiguration这个类的对象的时候，请先调用此方法。
+	 * 需要填充的参数有name，maxGenerationNum,selectionRange,accuracy,OperatorConfiguration中的所有属性，
+	 * individualConfiguration中的individualNumber，GeneConfiguration中的NormalGeneHeaderLength,NormalGeneNum,
+	 * HomeoticGeneHeaderLength,HomeoticGeneNum，及GeneConfiguration中所包含的函数集
+	 * 此方法会改变传入的参数。
+	 * @param gepAlgConfiguration
+	 * @return
+	 */
+	public GepAlgConfiguration setGepAlgConfiguration(GepAlgConfiguration gepAlgConfiguration,DataSet dataSet);
 }
--- SF4GEP/src/domain/iservice/algOutput/IAlgRunStep.java
+++ SF4GEP/src/domain/iservice/algOutput/IAlgRunStep.java
@@ -2,13 +2,14 @@ package domain.iservice.algOutput;
 
 import java.util.List;
 
+import domain.core.algInputDataProcess.DataSet;
 import domain.core.algOutput.FittedValue;
 import domain.core.algOutput.GepAlgRun;
 import domain.core.algOutput.Population;
 import domain.core.algconfiguration.GepAlgConfiguration;
 
 public interface IAlgRunStep {
-	public GepAlgRun create(GepAlgConfiguration gepAlgConfiguration);
+	public GepAlgRun create(GepAlgConfiguration gepAlgConfiguration, DataSet dataSet);
 	public List<FittedValue> calculateFittedValue(Population population);
 	public List<Float> calculateFitness(Population population);
 	public Population select(GepAlgRun gepAlgRun);
