38603643e2f7e0b5c41e15507b7f370bba957db6
shenyuancs@gmail.com <shenyuancs@gmail.com@0f924ae1-d416-614b-8bf3-34ffd1195aa7>
2012-07-13 02:38:08 +0000

    改变了run的参数

--- SF4GEP/src/domain/iservice/algOutput/IAlgOutputService.java
+++ SF4GEP/src/domain/iservice/algOutput/IAlgOutputService.java
@@ -3,12 +3,13 @@ package domain.iservice.algOutput;
 import java.io.File;
 import java.util.List;
 
+import domain.core.algInputDataProcess.DataSet;
 import domain.core.algOutput.GepAlgRun;
 import domain.core.algOutput.Individual;
 import domain.core.algconfiguration.GepAlgConfiguration;
 
 public interface IAlgOutputService {
-	public Individual run(GepAlgConfiguration gepAlgConfiguration, IAlgRunStep algRunStep);
+	public Individual run(GepAlgConfiguration gepAlgConfiguration, IAlgRunStep algRunStep, DataSet dataSet);
 	public List<Float> getMaxFitnessInEveryGeneration(GepAlgRun gepAlgRun);
 	public List<Float> getMinFitnessInEveryGeneration(GepAlgRun gepAlgRun);
 	public boolean writeToFile(File file);
