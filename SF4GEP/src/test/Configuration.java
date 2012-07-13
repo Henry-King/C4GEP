0215ab587ee43674a10560e8c0e3e0c97b311af4
shenyuancs@gmail.com <shenyuancs@gmail.com@0f924ae1-d416-614b-8bf3-34ffd1195aa7>
2012-07-13 01:50:34 +0000

    配置子系统的使用方法

---
+++ SF4GEP/src/test/Configuration.java
@@ -0,0 +1,64 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import jxl.read.biff.BiffException;
+
+import domain.core.algInputDataProcess.DataSet;
+import domain.core.algconfiguration.GeneConfiguration;
+import domain.core.algconfiguration.GepAlgConfiguration;
+import domain.core.algconfiguration.IndividualConfiguration;
+import domain.core.algconfiguration.OperatorConfiguration;
+import domain.core.algconfiguration.function.Additioin;
+import domain.core.algconfiguration.function.Divide;
+import domain.core.algconfiguration.function.Minus;
+import domain.core.algconfiguration.function.Multiply;
+import domain.iservice.algConfiguration.IgepConfigurationService;
+import domain.iservice.algInputDataProcess.IDataInputService;
+import domain.service.algConfiguration.GepConfigurationService;
+import domain.service.algInputDataProcess.DataInputService;
+
+public class Configuration {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws BiffException 
+	 */
+	public static void main(String[] args) throws BiffException, IOException {
+		// TODO Auto-generated method stub
+		IDataInputService dataInputService=new DataInputService();
+		DataSet dataSet=dataInputService.processInputDataSet(new File("InputDemo.xls"));
+		GepAlgConfiguration gepAlgConfiguration=new GepAlgConfiguration();
+		gepAlgConfiguration.setAccuracy((float) 0.01);
+		gepAlgConfiguration.setSelectionRange((float) 100);
+		gepAlgConfiguration.setName("≤‚ ‘");
+		gepAlgConfiguration.setMaxGeneration((long) 10000);
+		IndividualConfiguration individualConfiguration=new IndividualConfiguration();
+		individualConfiguration.setIndividualNumber(20);
+		GeneConfiguration geneConfiguration=new GeneConfiguration();
+		geneConfiguration.setHomeoticGeneHeaderLength(5);
+		geneConfiguration.setHomeoticGeneNumber(10);
+		geneConfiguration.setNormalGeneHeaderLength(7);
+		geneConfiguration.setNormalGeneNumber(3);
+		geneConfiguration.setFunctionUsed(Arrays.asList(new Additioin(),new Minus(),new Multiply(),new Divide()));
+		individualConfiguration.setGeneConfiguration(geneConfiguration);
+		gepAlgConfiguration.setIndividualConfiguration(individualConfiguration);	
+		OperatorConfiguration operatorConfiguration=new OperatorConfiguration();
+		operatorConfiguration.setGeneRecombineRate((float) 0.1);
+		operatorConfiguration.setGeneTransportRate((float) 0.1);
+		operatorConfiguration.setIsElement(new int[]{1,2,3});
+		operatorConfiguration.setIsTransportRate((float) 0.1);
+		operatorConfiguration.setMutateRate((float) 0.0444);
+		operatorConfiguration.setOnePointRecombineRate((float) 0.4);
+		operatorConfiguration.setRisElement(new int[]{1,2,3});
+		operatorConfiguration.setRisTransportRate((float) 0.1);
+		operatorConfiguration.setTwoPointRecombineRate((float) 0.2);
+		gepAlgConfiguration.setOperatorConfiguration(operatorConfiguration);
+		IgepConfigurationService gepConfigurationService=new GepConfigurationService();
+		gepAlgConfiguration=gepConfigurationService.setGepAlgConfiguration(gepAlgConfiguration, dataSet);
+	}
+
+}
