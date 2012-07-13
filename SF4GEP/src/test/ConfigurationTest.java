7e67c91e39f13023be6e1c75ea193a5c6a3cae5e
shenyuancs@gmail.com <shenyuancs@gmail.com@0f924ae1-d416-614b-8bf3-34ffd1195aa7>
2012-07-13 02:32:54 +0000

    修复bug，现在只能执行初始化种群

--- SF4GEP/src/test/ConfigurationTest.java
+++ SF4GEP/src/test/ConfigurationTest.java
@@ -1,62 +1,73 @@
 package test;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.util.Arrays;
 
 import jxl.read.biff.BiffException;
 
-import domain.core.algmodel.genepiece.Function;
-import domain.core.outputmodel.GepConfiguration;
-import domain.iservice.IgepAlgService;
-import domain.service.alg.baseclass.GepAlgService;
+import domain.core.algInputDataProcess.DataSet;
+import domain.core.algconfiguration.GeneConfiguration;
+import domain.core.algconfiguration.GepAlgConfiguration;
+import domain.core.algconfiguration.IndividualConfiguration;
+import domain.core.algconfiguration.OperatorConfiguration;
+import domain.core.algconfiguration.function.Additioin;
+import domain.core.algconfiguration.function.Divide;
+import domain.core.algconfiguration.function.Minus;
+import domain.core.algconfiguration.function.Multiply;
+import domain.iservice.algConfiguration.IgepConfigurationService;
+import domain.iservice.algInputDataProcess.IDataInputService;
+import domain.iservice.algOutput.IAlgOutputService;
+import domain.iservice.algOutput.IAlgRunStep;
+import domain.service.algConfiguration.GepConfigurationService;
+import domain.service.algInputDataProcess.DataInputService;
+import domain.service.algOutput.AlgOutputService;
+import domain.service.algOutput.AlgRunStep;
 
 public class ConfigurationTest {
-	public static void main(String argv[]) throws BiffException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException{
-		IgepAlgService myGepService=iniAlg();
-		System.out.println(myGepService.getAvailableCalculator().get(0).toString());
-		System.out.println(myGepService.getAvailableCreator().get(0).toString());
-		System.out.println(myGepService.getAvailableFunctions().get(0).toString());
-		System.out.println(myGepService.getAvailableFunctions().get(1).toString());
-		System.out.println(myGepService.getAvailableFunctions().get(2).toString());
-		System.out.println(myGepService.getAvailableFunctions().get(3).toString());
-		System.out.println(myGepService.getAvailableModifyings().get(0).toString());
-		System.out.println(myGepService.getAvailableSelector().get(0).toString());
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws BiffException 
+	 */
+	public static void main(String[] args) throws BiffException, IOException {
+		// TODO Auto-generated method stub
+		IDataInputService dataInputService=new DataInputService();
+		DataSet dataSet=dataInputService.processInputDataSet(new File("InputDemo.xls"));
+		GepAlgConfiguration gepAlgConfiguration=new GepAlgConfiguration();
+		gepAlgConfiguration.setAccuracy((float) 0.01);
+		gepAlgConfiguration.setSelectionRange((float) 100);
+		gepAlgConfiguration.setName("≤‚ ‘");
+		gepAlgConfiguration.setMaxGeneration((long) 10000);
+		IndividualConfiguration individualConfiguration=new IndividualConfiguration();
+		individualConfiguration.setIndividualNumber(20);
+		GeneConfiguration geneConfiguration=new GeneConfiguration();
+		geneConfiguration.setHomeoticGeneHeaderLength(5);
+		geneConfiguration.setHomeoticGeneNumber(10);
+		geneConfiguration.setNormalGeneHeaderLength(7);
+		geneConfiguration.setNormalGeneNumber(3);
+		geneConfiguration.setFunctionUsed(Arrays.asList(new Additioin(),new Minus(),new Multiply(),new Divide()));
+		individualConfiguration.setGeneConfiguration(geneConfiguration);
+		gepAlgConfiguration.setIndividualConfiguration(individualConfiguration);	
+		OperatorConfiguration operatorConfiguration=new OperatorConfiguration();
+		operatorConfiguration.setGeneRecombineRate((float) 0.1);
+		operatorConfiguration.setGeneTransportRate((float) 0.1);
+		operatorConfiguration.setIsElement(new int[]{1,2,3});
+		operatorConfiguration.setIsTransportRate((float) 0.1);
+		operatorConfiguration.setMutateRate((float) 0.0444);
+		operatorConfiguration.setOnePointRecombineRate((float) 0.4);
+		operatorConfiguration.setRisElement(new int[]{1,2,3});
+		operatorConfiguration.setRisTransportRate((float) 0.1);
+		operatorConfiguration.setTwoPointRecombineRate((float) 0.2);
+		gepAlgConfiguration.setOperatorConfiguration(operatorConfiguration);
+		IgepConfigurationService gepConfigurationService=new GepConfigurationService();
+		gepAlgConfiguration=gepConfigurationService.setGepAlgConfiguration(gepAlgConfiguration, dataSet);
+		run(gepAlgConfiguration,dataSet);
 	}
-	static IgepAlgService iniAlg() throws BiffException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException{
-		GepConfiguration myParameter=new GepConfiguration();
-		myParameter.setInputFile("InputDemo.xls");
-		myParameter.setName("≤‚ ‘123333");
-		myParameter.setAccuray("0.01");
-		myParameter.setNormalGeneNumber("3");
-		myParameter.setGeneRecombineRate("0.1");
-		myParameter.setGeneTransportRate("0.1");
-		myParameter.setNormalHeaderLength("7");
-		myParameter.setIsTransportRate("0.1");
-		myParameter.setMaxGeneration("100000");
-		myParameter.setOnePointRecombineRate("0.4");
-		myParameter.setPopulationSize("20");
-		myParameter.setRisTransportRate("0.1");
-		myParameter.setSelectionRange("100");
-		myParameter.setTwoPointRecombineRate("0.2");
-		myParameter.setMutateRate("0.044");
-		myParameter.setIsElement("1,2,3");
-		myParameter.setRisElement("1,2,3");
-		myParameter.setHomeoticGeneNumber("1");
-		myParameter.setHomeoticHeaderLength("5");
-		IgepAlgService myGepService=new GepAlgService();	
-		myParameter.setCreator(myGepService.getAvailableCreator().get(0).getClass().getName());
-		myParameter.setCalculator(myGepService.getAvailableCalculator().get(0).getClass().getName());
-		myParameter.setModify(myGepService.getAvailableModifyings().get(0).getClass().getName());
-		myParameter.setFunctionList(getFunctionList(myGepService));
-		myParameter.setSelector(myGepService.getAvailableSelector().get(0).getClass().getName());
-		myGepService.setParameters(myParameter);
-		return myGepService;
-	}
-	private static String getFunctionList(IgepAlgService myGepService) throws ClassNotFoundException, InstantiationException, IllegalAccessException{
-		List<Function> myFunction=myGepService.getAvailableFunctions();
-		StringBuffer result=new StringBuffer();
-		for(Function function:myFunction)
-			result.append(function.getClass().getName()+",");
-		return result.toString();
+	private static void run(GepAlgConfiguration gepAlgConfiguration,DataSet dataSet){
+		IAlgOutputService algOutputService=new AlgOutputService();
+		IAlgRunStep runStep=new AlgRunStep();
+		algOutputService.run(gepAlgConfiguration, runStep, dataSet);
 	}
 }
